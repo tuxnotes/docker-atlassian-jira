bd48674ac2255cfd666ddac82b7a089b950d6179
Dave Chevell <dchevell@atlassian.com>
2019-08-23 18:06:09 +1000

    Simplified JWT generation

--- bin/push-readme.py
+++ bin/push-readme.py
@@ -10,22 +10,19 @@ logging.basicConfig(level=logging.INFO)
 DOCKER_REPO = os.environ.get('DOCKER_REPO')
 DOCKER_USERNAME = os.environ.get('DOCKER_USERNAME')
 DOCKER_PASSWORD = os.environ.get('DOCKER_PASSWORD')
-DOCKER_TOKEN = os.environ.get('DOCKER_TOKEN')
 README_FILE = os.environ.get('README_FILE') or 'README.md'
 
 
-if DOCKER_TOKEN is None:
-    logging.info('Generating Docker Hub JWT')
-    data = {'username': DOCKER_USERNAME, 'password': DOCKER_PASSWORD}
-    r = requests.post('https://hub.docker.com/v2/users/login/', json=data)
-    token = r.json().get('token')
-    DOCKER_TOKEN = os.environ.setdefault('DOCKER_TOKEN', token)
+logging.info('Generating Docker Hub JWT')
+data = {'username': DOCKER_USERNAME, 'password': DOCKER_PASSWORD}
+r = requests.post('https://hub.docker.com/v2/users/login/', json=data)
+docker_token = r.json().get('token')
 
 logging.info(f'Updating Docker Hub description for {DOCKER_REPO}')
 with open(README_FILE) as f:
     full_description = f.read()
 data = {'registry': 'registry-1.docker.io', 'full_description': full_description}
-headers = {'Authorization': f'JWT {DOCKER_TOKEN}'}
+headers = {'Authorization': f'JWT {docker_token}'}
 r = requests.patch(f'https://cloud.docker.com/v2/repositories/{DOCKER_REPO}/',
                    json=data, headers=headers)
 
