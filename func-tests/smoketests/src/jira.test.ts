3e179c14ad21859eb1d8474356829979ca6e3979
Steve Smith <ssmith@atlassian.com>
2021-01-07 16:32:58 +1100

    DCD-1084: Add check of status body.

--- func-tests/smoketests/src/jira.test.ts
+++ func-tests/smoketests/src/jira.test.ts
@@ -4,11 +4,14 @@ import supertest from 'supertest'
 let jira = "http://jira:8080/jira"
 
 describe('Get status', () => {
-  it('responds with OK', async (done) => {
+  it('Responds with RUNNING', async () => {
     supertest(jira)
       .get('/status')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(200, done)
-  });
-});
+      .expect(200)
+      .then(resp => {
+          expect(resp.body).toMatchObject({state: "RUNNING"})
+      })
+  })
+})
