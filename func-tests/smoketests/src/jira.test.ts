9db6a828b2f119d69e3f8eda65c6b0ed11834ca1
Adam Brokes <abrokes@atlassian.com>
2021-02-16 17:21:38 +1100

    DCKUBE-124: Add view issue REST test
    - the tests will fail now if there is a failed assertion
    - if Jira fails to start with 500, the startup script will stop
    - add initial DEVELOPMENT.md documentation
    - fix license field so npm won't fail
    - add check for Jira license

--- func-tests/smoketests/src/jira.test.ts
+++ func-tests/smoketests/src/jira.test.ts
@@ -1,17 +1,33 @@
+import request from "supertest";
+const jira = process.env["JIRA_BASEURL"] || "http://jira:8080/jira";
+const adminPassword = process.env["JIRA_ADMIN_PWD"] || "admin";
 
-import supertest from 'supertest'
-
-const jira = "http://jira:8080/jira"
-
-describe('Jira status endpoint', () => {
-  it('responds with RUNNING', async () => {
-    supertest(jira)
-      .get('/status')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
+describe("Jira status endpoint", () => {
+  it("responds with RUNNING", () =>
+    request(jira)
+      .get("/status")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
       .expect(200)
-      .then(resp => {
-          expect(resp.body).toMatchObject({state: "RUNNING"})
-      })
-  })
-})
+      .then((resp) => {
+        expect(resp.body).toMatchObject({ state: "RUNNING" });
+      }));
+});
+
+describe("Jira REST API", () => {
+  it("returns a specific issue", () =>
+    request(jira)
+      .get("/rest/agile/1.0/issue/KT-1")
+      .auth("admin", adminPassword)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((resp) => {
+        expect(resp.body.fields).toEqual(
+          expect.objectContaining({
+            summary:
+              "Kanban cards represent work items >> Click the \"KT-1\" link at the top of this card to show the Detail view - there's more on Kanban in the 'Description' section",
+          })
+        );
+      }));
+});
