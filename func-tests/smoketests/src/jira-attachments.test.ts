5213b89d10568dfb607c1c05c6b4225d4cce2b3f
Adam Brokes <abrokes@atlassian.com>
2021-02-18 17:31:07 +1100

    DCDKUBE-124: Add attachment test

--- func-tests/smoketests/src/jira-attachments.test.ts
+++ func-tests/smoketests/src/jira-attachments.test.ts
@@ -1,35 +1,48 @@
 import request from "supertest";
+import fs from "fs";
 const jira = process.env["JIRA_BASEURL"] || "http://jira:8080/jira";
 const adminPassword = process.env["JIRA_ADMIN_PWD"] || "admin";
 
-test("We can upload an attachment to Jira ticket", async () => {
+describe("Jira attachments", () => {
+  const filename = "mynewfile3.txt";
   let attachments: number;
-  await request(jira)
-    .get("/rest/agile/1.0/issue/KT-5")
-    .auth("admin", adminPassword)
-    .set("Accept", "application/json")
-    .expect("Content-Type", /json/)
-    .expect(200)
-    .then((resp) => {
-      attachments = resp.body.fields.attachment.length
-    });
 
-  await request(jira)
-    .post("/rest/api/2/issue/KT-5/attachments")
-    .auth("admin", adminPassword)
-    .type("form")
-    .set("X-Atlassian-Token", "no-check")
-    .attach('file', '/tmp/adobegc.log')
-    .expect("Content-Type", /json/)
-    .expect(200)
+  beforeAll(() => {
+    fs.writeFileSync(filename, "Hello content!");
+  });
+
+  test("Upload a new attachment to an issue", async () => {
+    await request(jira)
+      .get("/rest/agile/1.0/issue/KT-5")
+      .auth("admin", adminPassword)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((resp) => {
+        attachments = resp.body.fields.attachment.length;
+      });
 
     await request(jira)
-    .get("/rest/agile/1.0/issue/KT-5")
-    .auth("admin", adminPassword)
-    .set("Accept", "application/json")
-    .expect("Content-Type", /json/)
-    .expect(200)
-    .then((resp) => {
-      expect(resp.body.fields.attachment).toHaveLength(attachments + 1);
-    });
+      .post("/rest/api/2/issue/KT-5/attachments")
+      .auth("admin", adminPassword)
+      .type("form")
+      .set("X-Atlassian-Token", "no-check")
+      .attach("file", "/tmp/adobegc.log")
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    await request(jira)
+      .get("/rest/agile/1.0/issue/KT-5")
+      .auth("admin", adminPassword)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((resp) => {
+        expect(resp.body.fields.attachment).toHaveLength(attachments + 1);
+      });
+  });
+
+  afterAll(() => {
+    fs.unlinkSync(filename);
+  });
 });
