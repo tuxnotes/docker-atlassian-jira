5fd36d26beb940c471faa58d739df15b413cd531
Steve Smith <ssmith@atlassian.com>
2021-02-23 10:56:01 +1100

    DCKUBE-127: Intial working version of the template.

--- pipelines-generator.py
+++ pipelines-generator.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from jinja2 import Template
 from pathlib import Path
 import os
@@ -6,57 +8,67 @@ TEMPLATE_FILE = 'bitbucket-pipelines.yml.j2'
 
 DOCKERFILE_VERSION_ARG='JIRA_VERSION'
 
+CORE_REPOS = ['atlassian/jira-core']
+SOFTWARE_REPOS = ['atlassian/jira-software']
+SD_REPOS = ['atlassian/jira-servicemanagement', 'atlassian/jira-servicedesk']
+
 images = {
-    'software': {
+    'jira-software': {
         8: {
-            start_version: '7.13',
-            end_version: '9',
-            default_release: True,
-            extra_tag_suffixes: ['ubuntu']
+            'start_version': '7.13',
+            'end_version': '9',
+            'default_release': True,
+            'extra_tag_suffixes': ['ubuntu'],
+            'docker_repos': SOFTWARE_REPOS,
         },
         11: {
-            start_version: '8.2',
-            end_version: '9',
-            default_release: False,
-            base_image: 'adoptopenjdk:11-hotspot',
+            'start_version': '8.2',
+            'end_version': '9',
+            'default_release': False,
+            'base_image': 'adoptopenjdk:11-hotspot',
+            'docker_repos': SOFTWARE_REPOS,
         }
     },
-    'servicedesk': {
-        docker_repos: ['atlassian/jira-servicemanagement', 'atlassian/jira-servicedesk'],
+    'jira-servicedesk': {
         8: {
-            start_version: '3.16',
-            end_version: '5',
-            default_release: True,
-            extra_tag_suffixes: ['ubuntu']
+            'start_version': '3.16',
+            'end_version': '5',
+            'default_release': True,
+            'extra_tag_suffixes': ['ubuntu'],
+            'docker_repos': SD_REPOS,
         },
         11: {
-            start_version: '4.2',
-            end_version: '5',
-            default_release: False,
+            'mac_key': 'jira-servicedesk',
+            'start_version': '4.2',
+            'end_version': '5',
+            'default_release': False,
+            'docker_repos': SD_REPOS,
         }
     },
-    'core': {
+    'jira': {
         8: {
-            start_version: '7.13',
-            end_version: '9',
-            default_release: True,
-            extra_tag_suffixes: ['ubuntu']
+            'start_version': '7.13',
+            'end_version': '9',
+            'default_release': True,
+            'extra_tag_suffixes': ['ubuntu'],
+            'docker_repos': CORE_REPOS,
         },
         11: {
-            start_version: '8.2',
-            end_version: '9',
-            default_release: False,
-            base_image: 'adoptopenjdk:11-hotspot',
+            'mac_key': 'jira',
+            'start_version': '8.2',
+            'end_version': '9',
+            'default_release': False,
+            'base_image': 'adoptopenjdk:11-hotspot',
+            'docker_repos': CORE_REPOS,
         }
     }
 }
 
 
-
 def main():
     path = Path(os.path.join(os.path.dirname(__file__), TEMPLATE_FILE))
     template = Template(path.read_text())
-    generated_output = template.render(flavours=images)
+    generated_output = template.render(images=images)
 
     print(generated_output)
 
