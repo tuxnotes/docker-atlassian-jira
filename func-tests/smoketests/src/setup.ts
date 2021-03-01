ab6455afe2a74f996574909f2d5c1e225c538f39
Adam Brokes <abrokes@atlassian.com>
2021-03-01 17:15:30 +1100

    DCKUBE-125: Add global 60s timeout

--- func-tests/smoketests/src/setup.ts
+++ func-tests/smoketests/src/setup.ts
@@ -2,11 +2,12 @@ import request from "supertest";
 import { adminPassword, jiraBaseUrl } from "./config";
 
 module.exports = async () => {
+  jest.setTimeout(60000);
   console.log("Trigger issue re-index via Jira REST API")
-  // await request(jiraBaseUrl)
-  //   .post("/rest/api/2/reindex")
-  //   .query({ type: "FOREGROUND" })
-  //   .auth("admin", adminPassword)
-  //   .set("Content-Type", "application/json")
-  //   .expect(202);
+  await request(jiraBaseUrl)
+    .post("/rest/api/2/reindex")
+    .query({ type: "FOREGROUND" })
+    .auth("admin", adminPassword)
+    .set("Content-Type", "application/json")
+    .expect(202);
 };
