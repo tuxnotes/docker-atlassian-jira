e7a55dd18a9c5b0a07b69a68a8634defd2457dd8
Adam Brokes <abrokes@atlassian.com>
2021-03-02 17:03:01 +1100

    DCKUBE-125: Switch to Node v 14

--- func-tests/smoketests/src/setup.ts
+++ func-tests/smoketests/src/setup.ts
@@ -1,4 +1,8 @@
-import request from "supertest";
+const superagent = require('superagent');
+require('superagent-retry-delay')(superagent);
+
+const request = require('supertest');
+
 import { adminPassword, jiraBaseUrl } from "./config";
 
 module.exports = async () => {
@@ -9,4 +13,10 @@ module.exports = async () => {
     .auth("admin", adminPassword)
     .set("Content-Type", "application/json")
     .expect(202);
+  
+  await request(jiraBaseUrl)
+    .get("/rest/api/2/reindex/progress")
+    .auth("admin", adminPassword)
+    .retry(5, 3000, [503])
+    .expect(200);
 };
