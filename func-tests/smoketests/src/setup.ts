a8348c64d8b9c6677d165c662fcde812c594536f
Adam Brokes <abrokes@atlassian.com>
2021-03-02 17:45:43 +1100

    DCKUBE-125: Add indexing test and remove superagent-retry-delay

--- func-tests/smoketests/src/setup.ts
+++ func-tests/smoketests/src/setup.ts
@@ -1,22 +1,15 @@
-const superagent = require('superagent');
-require('superagent-retry-delay')(superagent);
-
-const request = require('supertest');
-
+import request from "supertest";
 import { adminPassword, jiraBaseUrl } from "./config";
 
 module.exports = async () => {
-  console.log("Trigger issue re-index via Jira REST API")
+  console.log("Trigger issue re-index via Jira REST API");
   await request(jiraBaseUrl)
     .post("/rest/api/2/reindex")
     .query({ type: "FOREGROUND" })
     .auth("admin", adminPassword)
     .set("Content-Type", "application/json")
     .expect(202);
-  
-  await request(jiraBaseUrl)
-    .get("/rest/api/2/reindex/progress")
-    .auth("admin", adminPassword)
-    .retry(5, 3000, [503])
-    .expect(200);
+
+  // Jira returns 503 HTTP code for a short period while reindexing is triggered
+  await new Promise((r) => setTimeout(r, 500));
 };
--- func-tests/smoketests/src/tests/jira-system.test.ts
+++ func-tests/smoketests/src/tests/jira-system.test.ts
@@ -7,7 +7,7 @@ test("Jira status endpoint responds with RUNNING", async () =>
     .set("Accept", "application/json")
     .expect("Content-Type", /json/)
     .expect(200)
-    .then((resp) => {
+    .then((resp: any) => {
       expect(resp.body).toMatchObject({ state: "RUNNING" });
     }));
 
@@ -17,7 +17,7 @@ test("Verify that all plugins are enabled", async () => {
     .auth("admin", adminPassword)
     .expect("Content-Type", /json/)
     .expect(200)
-    .then((resp) => {
+    .then((resp: any) => {
       const plugins = resp.body.plugins;
       // we shouldn't rely on precise number of plugins as this is subject to change
       expect(plugins.length).toBeGreaterThan(200);
@@ -28,18 +28,27 @@ test("Verify that all plugins are enabled", async () => {
     });
 });
 
-test("Verify that index is readable", async () => {
+test("Verify that index is readable and all the records from the database can be indexed", async () => {
+  await request(jiraBaseUrl)
+    .post("/rest/api/2/reindex")
+    .query({ type: "FOREGROUND" })
+    .auth("admin", adminPassword)
+    .set("Content-Type", "application/json")
+    .expect(202);
+
+  // Jira returns 503 HTTP code for a short period while reindexing is triggered
+  await new Promise(r => setTimeout(r, 500));
 
   await request(jiraBaseUrl)
     .get("/rest/api/2/index/summary")
     .auth("admin", adminPassword)
     .expect(200)
     .expect("Content-Type", /json/)
-    .then((resp) => {
+    .then((resp: any) => {
       expect(resp.body.issueIndex.indexReadable).toEqual(true);
       const countInDatabase = resp.body.issueIndex.countInDatabase;
       expect(countInDatabase).toBeGreaterThan(0);
-      // expect(resp.body.issueIndex.countInIndex).toEqual(countInDatabase); // there are potential inconsistencies (N+1 problem)
+      expect(resp.body.issueIndex.countInIndex).toEqual(countInDatabase); // there are potential inconsistencies (N+1 problem)
       expect(resp.body.issueIndex.countInIndex).toBeGreaterThan(0);
     });
 });
