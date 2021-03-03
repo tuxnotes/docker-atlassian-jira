d0e0b342a2b8e7266c7d6bfdae6e63ac096e2a29
Adam Brokes <abrokes@atlassian.com>
2021-03-03 17:13:37 +1100

    DCKUBE-125: Add smoke testing banner

--- func-tests/smoketests/src/setup.ts
+++ func-tests/smoketests/src/setup.ts
@@ -2,6 +2,13 @@ import request from "supertest";
 import { adminPassword, jiraBaseUrl } from "./config";
 
 module.exports = async () => {
+  // This can be used for a common setup - e.g. indexing (but Jira indexes after startup and upgrade)
+
+  console.log("");
+  console.log('_.~"~._.~"~._.~"~._.~"~._');
+  console.log(" Jira Docker smoke tests");
+  console.log('_.~"~._.~"~._.~"~._.~"~._');
+
   console.log("Trigger issue re-index via Jira REST API");
   await request(jiraBaseUrl)
     .post("/rest/api/2/reindex")
