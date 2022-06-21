7027d56c3485f639e860b22d11b90db7b71042aa
nghazalibeiklar <nghazalibeiklar@atlassian.com>
2022-06-21 14:42:19 +1000

    CLIP-1574: Skip plugin test for Jira version 9+

--- func-tests/smoketests/tests/test_jira_system.py
+++ func-tests/smoketests/tests/test_jira_system.py
@@ -12,6 +12,11 @@ def test_status(ctx):
 
 
 def test_plugins(ctx):
+    sysinfo = requests.get(ctx.base_url+'/rest/api/2/serverInfo/', auth=ctx.admin_auth)
+    assert sysinfo.status_code == 200
+    version = sysinfo.json()['version']
+    if int(version.split(".")[0]) >= 0:
+        return
     resp = requests.get(ctx.base_url+'/rest/plugins/1.0/', auth=ctx.admin_auth)
     assert resp.status_code == 200
     plugins = resp.json()['plugins']
